ffe955ec63d0dfcc870b155aea07eaae553df437
GregorioBlazquez <gregorio.blazquez@estudiante.uam.es>
2026-02-17 12:32:54 +0100

    Add test case with fiware-servicepath empty

---
+++ tests/cases/postgis/000_basic/004_empty_fiwareservicepath/setup.sql
@@ -0,0 +1,60 @@
+/*
+* Copyright 2026 Telefónica Soluciones de Informática y Comunicaciones de España, S.A.U.
+*
+* This file is part of kafnus-connect
+*
+* kafnus-connect is free software: you can redistribute it and/or
+* modify it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* kafnus-connect is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with kafnus. If not, see http://www.gnu.org/licenses/.
+*/
+
+-- Drop table
+DROP TABLE IF EXISTS test._sensor;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS test._sensor (
+    recvtime TIMESTAMPTZ NOT NULL DEFAULT now(),
+    fiwareservicepath TEXT,
+    entityid TEXT,
+    entitytype TEXT,
+    timeinstant TIMESTAMPTZ,
+    temperature DOUBLE PRECISION,
+    CONSTRAINT _sensor_pkey PRIMARY KEY (timeinstant, entityid)
+);
+
+-- Drop table
+DROP TABLE IF EXISTS test._sensor_lastdata;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS test._sensor_lastdata (
+    recvtime TIMESTAMPTZ NOT NULL DEFAULT now(),
+    fiwareservicepath TEXT,
+    entityid TEXT,
+    entitytype TEXT,
+    timeinstant TIMESTAMPTZ,
+    temperature DOUBLE PRECISION,
+    CONSTRAINT _sensor_lastdata_pkey PRIMARY KEY (entityid)
+);
+
+-- Drop table
+DROP TABLE IF EXISTS test._sensor_mutable;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS test._sensor_mutable (
+    recvtime TIMESTAMPTZ NOT NULL DEFAULT now(),
+    fiwareservicepath TEXT,
+    entityid TEXT,
+    entitytype TEXT,
+    timeinstant TIMESTAMPTZ,
+    temperature DOUBLE PRECISION,
+    CONSTRAINT _sensor_mutable_pkey PRIMARY KEY (timeinstant, entityid)
+);
